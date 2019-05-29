f2902b5325cfd53f44ee69d823754c672607b5b9
Pavanraju09 <34839254+Pavanraju09@users.noreply.github.com>
2019-05-29 12:56:17 +0530

    Update vgg_face_recognition.py

--- vgg_face_recognition.py
+++ vgg_face_recognition.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from keras.models import Model, Sequential
 from keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
 from PIL import Image
@@ -15,9 +9,6 @@ import matplotlib.pyplot as plt
 
 
 # # CNN Architecture
-
-# In[2]:
-
 
 model = Sequential()
 model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
@@ -68,22 +59,12 @@ model.add(Activation('softmax'))
 
 # # Adding weights to the model
 
-# In[3]:
-
-
 model.load_weights('C:/Users/Pavan Raju/Desktop/FACE-RECOGNITION/FaceRecognitoin/vgg_face_weights.h5')
-
-
-# In[4]:
-
 
 vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
 
 
 # # Creating necessary functions
-
-# In[5]:
-
 
 #to load images from the path
 def load_image(path):
@@ -92,6 +73,8 @@ def load_image(path):
     img = np.expand_dims(img, axis = 0)
     img = preprocess_input(img)
     return img
+
+
 #to load images from webcam
 def webcam_image(image):
     img = cv2.resize(image,(224,224))
@@ -133,37 +116,14 @@ employees = dict()
 database = create_database(database_path)
 database
 
-
-# In[38]:
-
-
-import cv2
-import requests
-
-
-#url = "http://mmqn6w:9779@ipphonecamera.deskshare.com/"
-#url = "http://192.168.43.1:8080/"
-
-'''json_data={"username":"mmqn6w","password":"9779","language":"en"}
-headers={
-    'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8*',
-    'Accept-Encoding':'gzip, deflate',
-    'Accept-Language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
-    'Referer': 'http://ipphonecamera.deskshare.com/'
-}'''
-
-#url = "http://netra:12345@10.194.178.222:8080/shot.jpg"
+#Creating a function for webcam detection
 
 def webcam_on(database):
     cap = cv2.VideoCapture(0)
-    #cap = cv2.VideoCapture("http://netra:12345@10.194.178.222:8080/shot.jpg")
+    
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     while True:
         ret,img = cap.read()
-        #img_req = requests.get(url)
-        #img_arr = np.array(bytearray(img_req.content), dtype=np.uint8)
-        #img = cv2.imdecode(img_arr, -1)
         faces = face_cascade.detectMultiScale(img,1.3,5)
         for (x,y,w,h) in faces:
             x1 = x
@@ -174,8 +134,6 @@ def webcam_on(database):
             detected_face = img[int(y1):int(y2), int(x1):int(x2)]
             detected_face = webcam_image(detected_face)
             captured_embedding = vgg_face_descriptor.predict(detected_face)[0,:] 
-            #encode = base64.b64encode(img)
-            #client.publish("netra123/test123",encode)
             for i in database:
                 person_name = i
                 person_embedding = database[person_name]
@@ -204,75 +162,6 @@ def webcam_on(database):
     cap.release()
     cv2.destroyAllWindows()
 
-
-# In[39]:
-
-
 webcam_on(database)
 
 
-"""
-import paho.mqtt.client as mqtt
-import base64
-from PIL import Image
-import io
-import cv2
-import numpy as np
-
-
-def on_connect(client,userdata,flags,rc):
-    print("connected with code : ",str(rc))
-    client.subscribe("netra123/query")
-
-def on_message(client,userdata,msg):
-    decode = base64.b64decode(msg.payload)
-    print("***")
-    img = Image.open(io.BytesIO(decode))
-    img = np.array(img)
-    cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    captured_img = webcam_image(img)
-    captured_emb = vgg_face_descriptor.predict(captured_img)[0,:]
-    for i in database:
-        person_name = i
-        person_embedding = database[person_name][0:]
-        euc_distance = EuclideanDistance(person_embedding,captured_emb)
-        cos_distance = CousineDistance(person_embedding,captured_emb)
-        if cos_distance < 0.3 :
-            #print("Euclidean_distance = ",euc_distance)
-            #print("Cosine_distance = ",cos_distance)
-            print("Detected face is ", person_name)
-            client.publish("netra123/result",person_name)
-    
-    
-client = mqtt.Client()
-client.connect("broker.hivemq.com",1883,60)
-client.on_connect = on_connect
-client.on_message = on_message
-client.loop_forever()
-
-client = mqtt.Client()
-client.connect("broker.hivemq.com",1883,60)
-
-cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-while cap.isOpened():
-    ret, img = cap.read()
-    faces = face_cascade.detectMultiScale(img,1.3,5)
-    for (x,y,w,h) in faces:
-        x1 = x
-        y1 = y
-        x2 = x+w  
-        y2 = y+h 
-        img = cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
-        detected_face = img[int(y1):int(y2), int(x1):int(x2)]
-        cv2.imwrite("sample.jpg",detected_face)
-        encode_image = open("sample.jpg",'rb')
-        output_image = encode_image.read()
-        output = base64.b64encode(output_image)
-        client.publish("netra123/test123",output)
-    cv2.imshow("image",img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
-"""
